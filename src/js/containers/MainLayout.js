4e75e59ee69f8104c8f1b9788af3dc6311bc0742
Steve Juney <smrt.local@gmail.com>
2017-09-04 12:09:45 +0300

    playground clearify

--- src/js/containers/MainLayout.js
+++ src/js/containers/MainLayout.js
@@ -1,33 +1,11 @@
 import React from 'react';
 
-import Header from '../components/Header';
-import Posts from '../components/Posts';
-
 export default class MainLayout extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            posts: [
-                {title: "react"},
-                {title: "react-router"},
-                {title: "redux"},
-                {title: "jsx"},
-                {title: "virtual-dom"},
-                {title: "components"}
-            ]
-        };
-    }
-
-
     render() {
         return (
             <div className="wrapper" onClick="">
-                <h1>React Lesson: 1 [ Components, JSX, state & props ]</h1>
-                <Header />
+                <h1>React Lesson: 1 [ Playgroound ]</h1>
 
-                <Posts posts={ this.state.posts } />
             </div>
         );
     }

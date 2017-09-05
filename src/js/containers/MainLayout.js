885c9f0ebee94b662c954ae2d15b443b50839b9d
Steve Juney <smrt.local@gmail.com>
2017-09-05 16:18:43 +0300

    small fix

--- src/js/containers/MainLayout.js
+++ src/js/containers/MainLayout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class MainLayout extends React.Component {
     render() {
         return (
-            <div className="wrapper" onClick="">
+            <div className="wrapper">
                 <h1>[ Playground ]</h1>
 
                 <p>Начало: сейчас мы сделаем самый простой компонент меню, который можно будет скрыть и отобразить.</p>

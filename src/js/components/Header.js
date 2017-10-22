974786ddc28ba5ff8d44183930d78773bdbe5fd8
Steve Juney <smrt.local@gmail.com>
2017-10-22 14:38:43 +0300

    Update tasks info

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = (props) => (
     <header className="header">
-        <h1 className="page-title">React Lesson: 4 [ context, ?Children API? ]</h1>
+        <h1 className="page-title">React Lesson: 4 [ context, Children API ]</h1>
         <h3 className="description">
             Задания на сегодня:
         </h3>
@@ -13,12 +13,13 @@ const Header = (props) => (
                 Создать компонент меню, с рабочими пунктами - "создать пост", инпут поиска (3 балла) и главная на
                 которой должны отображатся все посты. ({`<Link/>`})
             </li>
-            <li>Создать компонент инпута внешний вид которого будет зависеть от родительского компонента. [`context`] (3 балла)</li>
+            <li>Создать компонент инпута внешний вид которого будет зависеть от родительского компонента. [`context`] (не обязательно)</li>
             <li>
                 Создать компонент Notify который будет отвечать за оповещение
                 о том что ваш пост был успешно создан. Компонент должен быть `position: fixed` и удалятся из DOM через время.
             </li>
-
+            <li>Сделать слайдер с использованием Children API</li>
+            <li>Дополнительно: Использовать модуль react-transition-group для более плавной анимации слайдера.</li>
         </ul>
     </header>
 );

dff79e47134a3c3b3a175dc6bcec7c3d01e614c3
Maya P <maya.pogrebnyak@gmail.com>
2017-10-23 16:30:49 +0300

    Me u changes

--- src/js/containers/MainLayout.js
+++ src/js/containers/MainLayout.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Menu from '../components/Menu';
 
 export default class MainLayout extends React.Component {
+    
     render() {
+        let bla = ['Home', 'About us', 'Contacts', 'Articles'];
         return (
             <div className="wrapper">
                 <h1>[ Playground ]</h1>
 
                 <p>Начало: сейчас мы сделаем самый простой компонент меню, который можно будет скрыть и отобразить.</p>
+                <ul>
+                    <Menu data={bla} />
+                    
+                    
+                </ul>
             </div>
         );
+
     }
 }

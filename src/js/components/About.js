3089677b9e887f0025ad902ac4dea8c3b22618c2
Steve Juney <smrt.local@gmail.com>
2017-10-27 01:36:53 +0300

    update page 'about'

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -3,9 +3,7 @@ import React from 'react';
 const About = props => (
     <div className="about-container">
         <p>
-            Hi there! This is simple react `blog` without any backend. Created for 
-            <a href="https://github.com/uppermanis/react-course" target="_blank">react-course</a>
-            by <a href="https://github.com/uppermanis" target="_blank">me</a>.
+            Hi there! This is simple react `blog` without any backend. Created for <a href="https://github.com/uppermanis/react-course" target="_blank">react-course</a> by <a href="https://github.com/uppermanis" target="_blank">me</a>.
         </p>
     </div>
 )
